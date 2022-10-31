d03d273eedb6c654c7f1c6182396673994784aa6
natt_nawarat <44581946+NattNawarat@users.noreply.github.com>
2022-10-31 21:48:40 +0700

    Time: 12 ms (67.61%), Space: 13.7 MB (71.44%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int Max = nums.at(0);
+        int n = nums.size();
+        int curr_prod = nums.at(0);
+        for(int i = 1 ; i < n ;i++){
+            curr_prod *= nums.at(i);
+            if (curr_prod == 0){
+                curr_prod = nums.at(i);
+                Max = max(0,Max);
+            }
+            Max = max(curr_prod,Max);
+        }
+        curr_prod = nums.at(n-1);
+        Max = max(Max,curr_prod);
+        for(int i = n-2 ; i >= 0 ;i--){
+            curr_prod *= nums.at(i);
+            if (curr_prod == 0){
+                curr_prod = nums.at(i);
+                Max = max(0,Max);
+            }
+            Max = max(curr_prod,Max);
+        }
+    return Max;
+    }
+};
