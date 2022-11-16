9ed6e7f0460b3d00ed19428b7d995d50cf9511ac
natt_nawarat <44581946+NattNawarat@users.noreply.github.com>
2022-11-16 23:23:47 +0700

    Time: 2302 ms (5.00%), Space: 355.7 MB (6.77%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        queue<pair<int,int>> BFS;//(index,jumps)
+        BFS.push(make_pair(0,0));
+        pair<int,int> curr_node;
+        int ans = 0;
+        bool visit[n];
+        for(int i = 0 ; i < n ;i++){
+            visit[i] = false;
+        }
+        if(n==1) return 0;
+        while(!BFS.empty()){
+            curr_node = BFS.front();
+            BFS.pop();
+            int curr_pos = curr_node.first;
+            if(visit[curr_pos]) continue;
+            int jump_length = nums[curr_pos];
+            int number_of_jump = curr_node.second;
+            visit[curr_pos] = true;
+            printf("%d %d %d\n",curr_pos,curr_pos + jump_length,number_of_jump);
+            if(curr_pos + jump_length >= n-1)
+            {
+                ans = number_of_jump+1;
+                break;
+            }
+            for(int i = curr_pos+1; i <= curr_pos + jump_length;i++){
+                BFS.push(make_pair(i,number_of_jump+1));
+            }
+        }
+        return ans;
+    }
+};
