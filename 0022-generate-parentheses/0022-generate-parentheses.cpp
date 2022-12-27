1db017b13392d992ea5e9633a99941d9dabbeb69
natt_nawarat <44581946+NattNawarat@users.noreply.github.com>
2022-12-27 11:27:17 +0700

    Time: 4 ms (74.87%), Space: 7.6 MB (94.85%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> ans;
+    void addParenthesis(int n,int l,int r, string s){
+        string new_s;
+        //cout<< l<<" "<<r<<" "<<s<<endl;
+        if (l == 0 && r == 1 ){
+            ans.push_back(s+")");
+            return;
+        }
+        if (l > 0){
+            new_s = s+"(";
+            addParenthesis(n,l-1,r,new_s);        
+        }
+        if (r > l){
+            new_s = s+")";
+            addParenthesis(n,l,r-1,new_s);
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        addParenthesis(n,n,n,"");
+        int N = ans.size();
+        for(int i = 0 ; i < N ; i ++){
+            cout<< ans.at(i)<<endl;
+        }
+        return ans;
+    }
+};
