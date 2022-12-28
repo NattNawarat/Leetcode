95b7e7dbf00d4dde8e58ef09c7f7d945b6b9f024
natt_nawarat <44581946+NattNawarat@users.noreply.github.com>
2022-12-28 15:32:10 +0700

    Time: 35 ms (26.43%), Space: 13.7 MB (16.05%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def addLetter(self,index,combination):
+        print("index",index,len(self.digits))
+        if (index == len(self.digits)):
+            self.combinations.append(combination)
+            return
+        
+        digit = self.digits[index]
+        
+        for char in self.digit_table[digit]:
+            print(index+1,combination+char)
+            self.addLetter(index + 1,combination+char)
+            
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        self.combinations = []
+        self.digits = digits
+        self.digit_table = {
+            '2' : ["a","b","c"],
+            '3' : ["d","e","f"],
+            '4' : ["g","h","i"],
+            '5' : ["j","k","l"],
+            '6' : ["m","n","o"],
+            '7' : ["p","q","r","s"],
+            '8' : ["t","u","v"],
+            '9' : ["w","x","y","z"],
+        }
+        if (len(self.digits) > 0):
+            self.addLetter(0,"")
+        return self.combinations
